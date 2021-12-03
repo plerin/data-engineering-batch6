c735d79bdbce7b214607a08390b4a6ac729603d1
keeyong <keeyonghan@hotmail.com>
2021-12-03 14:49:46 -0800

    Update NameGenderCSVtoRedshift_v2.py

--- dags/NameGenderCSVtoRedshift_v2.py
+++ dags/NameGenderCSVtoRedshift_v2.py
@@ -13,7 +13,7 @@ import psycopg2
 def get_Redshift_connection():
     host = "learnde.cduaw970ssvt.ap-northeast-2.redshift.amazonaws.com"
     redshift_user = "keeyong"  # 본인 ID 사용
-    redshift_pass = "KeeyongHan!1"  # 본인 Password 사용
+    redshift_pass = "..."  # 본인 Password 사용
     port = 5439
     dbname = "dev"
     conn = psycopg2.connect(f"dbname={dbname} user={redshift_user} host={host} password={redshift_pass} port={port}")
